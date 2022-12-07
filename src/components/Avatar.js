fa4bd7ae41152c4ed2ccf35aed8156b28095b887
naeem <naeemqg1@gmail.com>
2022-12-07 19:10:46 +0530

    emojipedia START

--- src/components/Avatar.js
+++
@@ -1,7 +0,0 @@
-const Avatar = (props)=>{
-    return <img className="circle-img"
-    src={props.img}
-    alt="avatar_img"
-  />
-};
-export default Avatar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "./index.css"
-
+import "./index.css";
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-
 root.render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>
 );
-
-//2. Extract the contact card as a reusable Card component.
-//3. Use props to render the default Beyonce contact card
-//so the Card component can be reused for other contacts.
-//4. Import the contacts.js file to create card components.
