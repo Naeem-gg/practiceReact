a019412ea44d8613a2c7241c4b2c61435ac7e69f
naeem <naeemqg1@gmail.com>
2022-12-07 22:51:03 +0530

    emojipedia END

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,26 @@
+import Heading from "./Heading";
+import Entry from "./Entry";
+import emoji from "./emojipedia";
+
+const oneEntry = (entry) =>{
+  return<Entry
+        key={entry.id}
+        name={entry.name}
+        emoji={entry.emoji}
+        meaning={entry.meaning}
+        />
+}
+
 const App = () => {
+
   return (
     <>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
+    <Heading/>
+      
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
-      </dl>
+        {emoji.map(oneEntry)}
+      </dl> 
     </>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+const Heading = ()=>{
+    return(
+        <h1>
+        <span>emojipedia</span>
+      </h1>
+    );
+};
+export default Heading;
