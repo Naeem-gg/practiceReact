841a79e4ad2e3446e95a79fea46cbc9f73a25e85
naeem <naeemqg1@gmail.com>
2022-12-07 19:04:15 +0530

    React DevTools END

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import Card from "./Card";
 import contacts from "./contacts";
+const createCard = (oneContact)=>{
+  return(
+    <Card 
+    name={oneContact.name} 
+    key={oneContact.id}
+    // id={oneContact.id}
+    phone={oneContact.phone}
+    email={oneContact.email}
+    img={oneContact.imgURL}
+    />
+    );
+  }
+
 const App = () => {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card name={contacts[0].name} img={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email}/>
-    <Card name={contacts[1].name} img={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email}/>
-    <Card name={contacts[2].name} img={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email}/>
+      {contacts.map(createCard)}
+      {/* <Card name={contacts[0].name} img={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email} />
+      <Card name={contacts[1].name} img={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email} />
+      <Card name={contacts[2].name} img={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email} /> */}
     </div>
   );
 }
---
+++ src/components/Avatar.js
@@ -0,0 +1,7 @@
+const Avatar = (props)=>{
+    return <img className="circle-img"
+    src={props.img}
+    alt="avatar_img"
+  />
+};
+export default Avatar;
