e901a5a5747a3c72d13f21b2f2219431bac7a6f4
naeem <naeemqg1@gmail.com>
2022-12-09 23:37:36 +0530

    custom

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,24 @@ import Heading from "./Heading";
 import Entry from "./Entry";
 import emoji from "./emojipedia";
 
-const oneEntry = (entry) =>{
-  return<Entry
-        key={entry.id}
-        name={entry.name}
-        emoji={entry.emoji}
-        meaning={entry.meaning}
-        />
-}
+
 
 const App = () => {
 
   return (
     <>
-    <Heading/>
-      
+      <Heading />
+
 
       <dl className="dictionary">
-        {emoji.map(oneEntry)}
-      </dl> 
+        {emoji.map(entry => <Entry
+            key={entry.id}
+            name={entry.name}
+            emoji={entry.emoji}
+            meaning={entry.meaning}
+          />
+        )}
+      </dl>
     </>
   );
 }
