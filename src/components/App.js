9d049b9c399d93885af808d172aacded342caca3
naeem <naeemqg1@gmail.com>
2022-12-10 23:10:16 +0530

    useState END

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-const App=()=> {
-  return (
+import { useState } from "react";
+const App = ()=>{
+  const [count,setState] = useState(0)
+  const increase = ()=>{
+    setState(count+1);
+  }
+  return(
     <div className="container">
-    <h1>0</h1>
-    <button>+</button>
-  </div>
+      <h1>{count}</h1>
+      <button onClick={()=>setState(count-1)}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
   );
 }
 
