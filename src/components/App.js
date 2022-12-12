dfbb4e2738eeecc6c89aa2f89f734e394f566c32
naeem <naeemqg1@gmail.com>
2022-12-12 23:59:33 +0530

    fistName lastName using useState Hook START

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
-import { useState } from "react"
-
 export default function App() {
-  const [styleobj,setStyle] = useState({});
-  const [name,setName] = useState("Hello");
-  const [btnName,setBtnName] = useState("");
-  const mouseOver = ()=>{
-    setStyle({backgroundColor:"black"})
-  }
-  const mouseOut = ()=>{
-    setStyle({backgroundColor:"white"})
-
-  }
-  return (
-    <div className="container">
-    <h1>{btnName || "Hello"}</h1>
-    <input type="text" placeholder="What's your name?" onChange={e=>setName(e.target.value)} />
-    <button onMouseOver={mouseOver} onMouseOut={mouseOut} style={styleobj} onClick={()=>{setBtnName(name)}}>Submit</button>
-  </div>
-  )
+  return (<div className="container">
+    <h1>Hello</h1>
+    <form>
+      <input name="fName" placeholder="First Name" />
+      <input name="lName" placeholder="Last Name" />
+      <button>Submit</button>
+    </form>
+  </div>);
 }
