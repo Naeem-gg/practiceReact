7b6fc7e9fc521c72cc645d8c312a43dc303e93b0
naeem <naeemqg1@gmail.com>
2022-12-13 00:17:11 +0530

    intermediate

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 export default function App() {
+  const [fName,setfName] = useState("");
+  const [lName,setlName] = useState("");
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const setNames = ()=>{ 
+    setFirstName(fName)
+    setLastName(lName)
+  }
+
   return (<div className="container">
-    <h1>Hello</h1>
-    <form>
-      <input name="fName" placeholder="First Name" />
-      <input name="lName" placeholder="Last Name" />
-      <button>Submit</button>
+    <h1>Hello {firstName} {lastName}</h1>
+    <form onSubmit={e=>e.preventDefault()}>
+      <input name="fName" placeholder="First Name" onChange={e=>setfName(e.target.value)} />
+      <input name="lName" placeholder="Last Name" onChange={e=>setlName(e.target.value)} />
+      <button onClick={setNames}>Submit</button>
     </form>
   </div>);
 }
