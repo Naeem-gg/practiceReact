53a1ad48ce2fb77c6dd8a058988dbf08cfbbd048
naeem <naeemqg1@gmail.com>
2022-12-14 22:09:41 +0530

    ternary operator modify

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 export default function List(props) {
-    
+
+    const [state,setState] = useState(false);
+    const change = ()=>{setState(p=>!p)}
   return (
     
-    <li>{props.item}</li>
+    <li style={{textDecoration:state?"line-through":"none"}} onClick={change}>{props.item}</li>
     
     )
 }
